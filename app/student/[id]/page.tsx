6b3ee91c0deb99de0d6d74f97be6316bd1bcdc4d
5ache3 <mohamedelkhache@gmail.com>
2025-02-13 19:56:32 +0000

    adding the header for the personal page

--- app/student/[id]/page.tsx
+++ app/student/[id]/page.tsx
@@ -60,10 +60,26 @@ export default function page() {
     <>
     <div  className="container m-auto">
         <NavBar/>
-        <div className='main'>
-            <div className='builtin p-1 bg-slate-200'>
+        <div className='main p-2'>
+            <div className='m-auto builtin p-1 max-w-screen-lg bg-slate-200'>
+            <div className='buirtin-header pr-3 pl-4'>
+            <table>
+                <tbody>
+                    <tr className='text-2xs lg:text-md font-semibold '>                                
+                        <td className='p-1 nom w-full '>Nom </td>
+                        <td className='flex gap-1'>
+                        <div className=' w-10 text-center md:w-20 md:h-15 text-wrap py-2 '>Note Tp</div>
+                        <div className=' w-10 text-center md:w-20 md:h-15  py-2 '>Note Devoir</div>
+                        <div className=' w-10 text-center md:w-20 md:h-15  py-2 '>Note Examen</div>
+                        <div className=' w-10 text-center md:w-20 md:h-15  text-wrap py-2 '>Note Finale</div>
+                        <div className=' w-10 text-center md:w-20 md:h-15  text-clip py-2'><span className=' hidden md:block'>Observation</span></div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
             {moduleRes.map((item)=>(
-                <div className='p-2 ' key={item.n}>
+                <div className='p-2 pt-1' key={item.n}>
                 <div className='bg-slate-100 rounded-md p-1 shadow-xl'>
                     <div className='matieres bg-slate-50 rounded-sd shadow-xl'>
                         <table>
@@ -74,11 +90,11 @@ export default function page() {
                                 <tr key={mat.r} className='text-xs font-semibold  md:text-lg'>                                
                                     <td className='p-1 nom w-full '><Link href={`/${info?.fil}/matieres/${mat.r}`} className='text-blue-950 hover:underline hover:text-blue-400'>{mat.mat_n}</Link></td>
                                     <td className='flex gap-1'>
-                                    <div className=' w-10 md:w-20 h-10 md:h-15 text-center py-2 '>{mat.nt}</div>
-                                    <div className=' w-10 md:w-20 h-10 md:h-15 text-center py-2 '>{mat.nd}</div>
-                                    <div className=' w-10 md:w-20 h-10 md:h-15 text-center py-2 '>{mat.ne}</div>
-                                    <div className=' w-10 md:w-20 h-10 md:h-15 text-center py-2 '>{mat.nf}</div>
-                                    <div className=' w-7 md:w-15 h-10 md:h-15 text-center py-2 '>{mat.d}</div>
+                                    <div className=' w-10 text-center md:w-20 h-10 md:h-15 py-2 '>{mat.nt}</div>
+                                    <div className=' w-10 text-center md:w-20 h-10 md:h-15 py-2 '>{mat.nd}</div>
+                                    <div className=' w-10 text-center md:w-20 h-10 md:h-15 py-2 '>{mat.ne}</div>
+                                    <div className=' w-10 text-center md:w-20 h-10 md:h-15 py-2 '>{mat.nf}</div>
+                                    <div className=' w-10 md:w-20 text-center md:w-15 h-10 md:h-15 py-2 '>{mat.d}</div>
                                     </td>
                                 </tr>
                             ))}
@@ -99,6 +115,25 @@ export default function page() {
                 </div>
                 </div>
             ))}
+            </div>
+            <div className='card mt-2 m-auto max-w-[500px] bg-slate-100 rounded-lg shadow-2xl text-sm font-sans font-semibold p-5'>
+                <div className='elements flex flex-col'>
+                        <div>
+                            <span className='text-gray-500'> Matricule:</span> {info?.mat}
+                        </div>
+                        <div>
+                            <span className='text-gray-500'> Nom:</span> {info?.name}
+                        </div>
+                        <div>
+                            <span className='text-gray-500'> Filliere:</span><Link href={`/${info?.fil}`} className='hover:underline hover:text-blue-400'>{info?.filliere}</Link> 
+                        </div>
+                        <div>
+                            <span className='text-gray-500'> Moyenne:</span> {info?.moyenne}
+                        </div>
+                        <div>
+                            <span className='text-gray-500'> Observation:</span> {info?.d}
+                        </div>
+                </div>
             </div>
         </div>
     </div>
