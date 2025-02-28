531f278c679a129aeb77bef5d2ca5a63d328790f
5ache3 <mohamedelkhache@gmail.com>
2025-02-28 20:50:15 +0000

    d

--- app/api/fillieres/list/route.js
+++ app/api/fillieres/list/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
     try{
         const db = await createConnection()
-        const query="SELECT DISTINCT filliere as L,fil as l FROM etudiants ORDER BY filliere";
+        const query="SELECT DISTINCT filliere as L,fil as l FROM semestres ORDER BY filliere";
         const [response]= await db.query(query);
         return NextResponse.json(response)
     }catch(error){
